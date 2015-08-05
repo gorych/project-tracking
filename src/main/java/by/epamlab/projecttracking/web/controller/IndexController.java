da1d14b8e49cf058c7ea3f786f111decff3929ea
Egor Semenchenia <gorych911@gmail.com>
2015-08-05 13:37:17 +0300

    added index controller

---
+++ src/main/java/by/epamlab/projecttracking/web/controller/IndexController.java
@@ -0,0 +1,21 @@
+package by.epamlab.projecttracking.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping({"/", "/login"})
+    public String goToLogin() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public String goToDashboard() {
+        return "dashboard";
+    }
+
+}
