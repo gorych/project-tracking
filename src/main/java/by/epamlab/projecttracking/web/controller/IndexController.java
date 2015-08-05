3a7db06ecbc8aa758735ab1f965276f1b8a3904c
Egor Semenchenia <gorych911@gmail.com>
2015-08-05 18:29:05 +0300

    added method sendLoginForm

--- src/main/java/by/epamlab/projecttracking/web/controller/IndexController.java
+++ src/main/java/by/epamlab/projecttracking/web/controller/IndexController.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping({"/", "/login"})
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String goToLogin() {
         return "login";
     }
 
-    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String sendLoginForm() {
+        return "dashboard";
+    }
+
+    @RequestMapping(value = {"/", "/dashboard"}, method = RequestMethod.GET)
     public String goToDashboard() {
         return "dashboard";
     }
