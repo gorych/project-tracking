970ff200aa2d22c7a79f7b28beaa051a1baaba75
Egor Semenchenia <gorych911@gmail.com>
2015-08-05 23:58:04 +0300

    updated goToLogin method

--- src/main/java/by/epamlab/projecttracking/web/controller/IndexController.java
+++ src/main/java/by/epamlab/projecttracking/web/controller/IndexController.java
@@ -3,13 +3,20 @@ package by.epamlab.projecttracking.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String goToLogin() {
-        return "login";
+    public ModelAndView goToLogin(@RequestParam(value = "auth_error", required = false) String error) {
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("auth_error", "Wrong login or password.");
+        }
+        model.setViewName("login");
+        return model;
     }
 
     @RequestMapping(value = {"/", "/dashboard"}, method = RequestMethod.GET)
