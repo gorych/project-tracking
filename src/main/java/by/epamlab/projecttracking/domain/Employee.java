7899c2d49a5a1413c2c750c0b772ce585a05b9fb
Egor Semenchenia <gorych911@gmail.com>
2015-08-05 18:27:32 +0300

    added getters for Employee

--- src/main/java/by/epamlab/projecttracking/domain/Employee.java
+++ src/main/java/by/epamlab/projecttracking/domain/Employee.java
@@ -27,24 +27,48 @@ public class Employee {
     @JoinColumn(name = "position_id")
     private Position position;
 
+    public int getId() {
+        return id;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
     }
 
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
+    public String getLastname() {
+        return lastname;
+    }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getLogin() {
+        return login;
     }
 
     public void setLogin(String login) {
         this.login = login;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Position getPosition() {
+        return position;
     }
 
     public void setPosition(Position position) {
