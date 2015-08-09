728426f9bdc92ace38554484f73e0d26aec154d8
Egor Semenchenia <gorych911@gmail.com>
2015-08-10 01:24:01 +0300

    Renamed and edited method goToDashboard to setModelParams.

--- src/main/java/by/epamlab/projecttracking/web/controller/UserController.java
+++ src/main/java/by/epamlab/projecttracking/web/controller/UserController.java
@@ -1,7 +1,10 @@
 package by.epamlab.projecttracking.web.controller;
 
 import by.epamlab.projecttracking.domain.Activity;
+import by.epamlab.projecttracking.domain.Member;
+import by.epamlab.projecttracking.domain.Task;
 import by.epamlab.projecttracking.service.interfaces.ActivityService;
+import by.epamlab.projecttracking.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,22 @@ public class UserController {
     @Autowired
     ActivityService activityService;
 
-    @RequestMapping(value = {"/", "/dashboard"}, method = RequestMethod.GET)
-    public ModelAndView goToDashboard() {
-        final int START_INDEX = 0;
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = {"/dashboard"}, method = RequestMethod.GET)
+    public ModelAndView setModelParams() {
+        final int FROM_INDEX = 0;
+        final int TO_INDEX = 5;
+
+        List<Activity> activities = activityService.getFromIndexToIndex(FROM_INDEX, TO_INDEX);
+        List<Task> tasks = userService.getUserTasks();
+        List<Member> members = userService.getMembers();
 
         ModelAndView model = new ModelAndView();
-        List<Activity> activities = activityService.getFiveElementsFromIndex(START_INDEX);
         model.addObject("activities", activities);
+        model.addObject("tasks", tasks);
+        model.addObject("members", members);
         model.setViewName("dashboard");
 
         return model;
@@ -33,9 +45,11 @@ public class UserController {
     @ResponseBody
     @RequestMapping(value = "/activity", method = RequestMethod.POST)
     public String loadActivity(@RequestParam(value = "fromIndex", required = false) Integer fromIndex) {
+        final int TO_INDEX = fromIndex + 5;
+
         String jsonString = "";
         if (fromIndex != null) {
-            List<Activity> activities = activityService.getFiveElementsFromIndex(fromIndex);
+            List<Activity> activities = activityService.getFromIndexToIndex(fromIndex, TO_INDEX);
             jsonString = activityService.getJsonString(activities);
         }
         return jsonString;
