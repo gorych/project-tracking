4132952b841c2cac0d262ed04f0557865177e49c
Egor Semenchenia <gorych911@gmail.com>
2015-08-09 17:52:13 +0300

    Adde jquery lib and JavaScript file.

---
+++ src/main/resources/js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var fromIndex = 5;
+
+    $('#more').click(function () {
+        $.ajax({
+            url: 'activity',
+            method: 'POST',
+            data: {"fromIndex": fromIndex}
+        }).done(function (data) {
+            var activities = JSON.parse(data);
+            if (data.length > 0) {
+                $.each(activities, function (index, element) {
+                    $(".activity").append("<div class='row'> <div class='date-and-time'>" + element["date"] +
+                        "</div> <span class='employee'>" + element["firstname"] + " " + element["lastname"] + "</span>"
+                        + element["comment"] + "</div>");
+                });
+                fromIndex += 5;
+            }
+        });
+    });
+});
