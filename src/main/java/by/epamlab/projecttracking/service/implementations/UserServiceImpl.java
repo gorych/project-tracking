7a580a5d5e231684032e69003d9286203064976e
Egor Semenchenia <gorych911@gmail.com>
2015-08-11 20:46:33 +0300

    deleted some methods

--- src/main/java/by/epamlab/projecttracking/service/implementations/UserServiceImpl.java
+++ src/main/java/by/epamlab/projecttracking/service/implementations/UserServiceImpl.java
@@ -1,16 +1,8 @@
 package by.epamlab.projecttracking.service.implementations;
 
-import by.epamlab.projecttracking.dao.interfaces.AssignmentDAO;
-import by.epamlab.projecttracking.dao.interfaces.EmployeeDAO;
-import by.epamlab.projecttracking.dao.interfaces.MemberDAO;
-import by.epamlab.projecttracking.dao.interfaces.TaskDAO;
-import by.epamlab.projecttracking.domain.Assignment;
 import by.epamlab.projecttracking.domain.Employee;
-import by.epamlab.projecttracking.domain.Member;
-import by.epamlab.projecttracking.domain.Task;
 import by.epamlab.projecttracking.security.UserRoleConstants;
 import by.epamlab.projecttracking.service.interfaces.EmployeeService;
-import by.epamlab.projecttracking.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,38 +11,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Service
-public class UserServiceImpl implements UserDetailsService, UserService {
+public class UserServiceImpl implements UserDetailsService {
 
     @Autowired
     EmployeeService employeeService;
-
-    @Autowired
-    EmployeeDAO employeeDAO;
-
-    @Autowired
-    MemberDAO memberDAO;
-
-    @Autowired
-    AssignmentDAO assignmentDAO;
-
-    @Autowired
-    TaskDAO taskDAO;
-
-    private String getUsername() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            return ((UserDetails) principal).getUsername();
-        }
-        return principal.toString();
-    }
 
     public UserDetails loadUserByUsername(String j_username) throws UsernameNotFoundException {
         Employee employee = employeeService.getByUsername(j_username);
@@ -67,29 +36,5 @@ public class UserServiceImpl implements UserDetailsService, UserService {
         return new User(j_username, employee.getPassword(), roles);
     }
 
-    @Transactional
-    public List<Task> getUserTasks() {
-        Employee employee = employeeDAO.getByUsername(getUsername());
-        List<Member> members = memberDAO.getByEmployee(employee);
 
-        List<Assignment> assignments = new ArrayList<>();
-        for (Member member : members) {
-            assignments.addAll(assignmentDAO.getByMember(member));
-        }
-
-        List<Task> tasks = new ArrayList<>();
-        for (Assignment assignment : assignments) {
-            int taskId = assignment.getTask().getId();
-            tasks.add(taskDAO.getById(taskId));
-        }
-
-        return tasks;
-    }
-
-    @Transactional
-    public List<Member> getMembers() {
-        Employee employee = employeeDAO.getByUsername(getUsername());
-        List<Member> members = memberDAO.getGroupByProject(employee);
-        return members;
-    }
 }
