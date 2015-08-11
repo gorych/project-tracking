90d5a16af648630cd9c49a3f68c0543bdcecfbc5
Egor Semenchenia <gorych911@gmail.com>
2015-08-11 15:12:15 +0300

    Added access denied method

--- src/main/java/by/epamlab/projecttracking/web/controllers/IndexController.java
+++ src/main/java/by/epamlab/projecttracking/web/controllers/IndexController.java
@@ -1,10 +1,15 @@
-package by.epamlab.projecttracking.web.controller;
+package by.epamlab.projecttracking.web.controllers;
 
+
+import by.epamlab.projecttracking.web.AttributeConstants;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 public class IndexController {
@@ -13,7 +18,7 @@ public class IndexController {
     public ModelAndView goToLogin(@RequestParam(value = "auth_error", required = false) String error) {
         ModelAndView model = new ModelAndView();
         if (error != null) {
-            model.addObject("auth_error", "Wrong username or password.");
+            model.addObject(AttributeConstants.AUTHENTICATION_ERROR, "Wrong username or password.");
         }
         model.setViewName("login");
         return model;
@@ -28,5 +33,19 @@ public class IndexController {
     public String goToAdminPage() {
         return "admin-page";
     }
+
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String accessDenied(Principal user, Model model) {
+        if (user != null) {
+            model.addAttribute(AttributeConstants.ACCESS_DENIED_ERROR, "Hi " + user.getName()
+                    + ", you do not have permission to access this page!");
+        } else {
+            model.addAttribute(AttributeConstants.ACCESS_DENIED_ERROR,
+                    "You do not have permission to access this page!");
+        }
+        return "403";
+
+    }
+
 
 }
