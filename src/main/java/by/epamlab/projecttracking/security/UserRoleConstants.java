f82695aad1fbed9c6db8a2dab7fd103e05baa3a9
Egor Semenchenia <gorych911@gmail.com>
2015-08-11 15:11:37 +0300

    Added user roles class constants

---
+++ src/main/java/by/epamlab/projecttracking/security/UserRoleConstants.java
@@ -0,0 +1,13 @@
+package by.epamlab.projecttracking.security;
+
+public final class UserRoleConstants {
+
+    public static final String ADMIN_ROLE_NAME = "admin";
+
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String USER = "ROLE_USER";
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    private UserRoleConstants(){ }
+
+}
