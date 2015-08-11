c61d4dc68afa73b262a99cbebe7afbfd0d70531b
Egor Semenchenia <gorych911@gmail.com>
2015-08-11 19:45:26 +0300

    added new constants

--- src/main/java/by/epamlab/projecttracking/web/AttributeConstants.java
+++ src/main/java/by/epamlab/projecttracking/web/AttributeConstants.java
@@ -4,12 +4,14 @@ public final class AttributeConstants {
 
     public static final String EMPLOYEE = "employee";
     public static final String TASKS = "tasks";
+    public static final String USER = "USER";
     public static final String MEMBERS = "members";
     public static final String ACTIVITIES = "activities";
     public static final String POSITIONS = "positions";
     public static final String USER_EXIST_ERROR = "user_exist_error";
     public static final String AUTHENTICATION_ERROR = "auth_error";
     public static final String ACCESS_DENIED_ERROR = "access_denied_error";
+    public static final String RESOURCE_NOT_FOUND_ERROR = "resource_not_found";
 
     private AttributeConstants(){ }
 
