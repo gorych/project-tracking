b7de1d7961b1a8ebf83e6a26a6e223e48654d64c
Egor Semenchenia <gorych911@gmail.com>
2015-08-13 00:38:00 +0300

    added new method goToCreateProjects

--- src/main/java/by/epamlab/projecttracking/web/controllers/AdminController.java
+++ src/main/java/by/epamlab/projecttracking/web/controllers/AdminController.java
@@ -1,10 +1,8 @@
 package by.epamlab.projecttracking.web.controllers;
 
-import by.epamlab.projecttracking.domain.Employee;
-import by.epamlab.projecttracking.domain.Position;
+import by.epamlab.projecttracking.domain.*;
 import by.epamlab.projecttracking.security.UserRoleConstants;
-import by.epamlab.projecttracking.service.interfaces.EmployeeService;
-import by.epamlab.projecttracking.service.interfaces.PositionService;
+import by.epamlab.projecttracking.service.interfaces.*;
 import by.epamlab.projecttracking.web.AttributeConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -26,12 +24,22 @@ public class AdminController {
     @Autowired
     PositionService positionService;
 
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    MemberService memberService;
+
     @Secured(UserRoleConstants.ADMIN)
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String createEmployee(Model model) {
         List<Position> positions = positionService.getAll();
         model.addAttribute(AttributeConstants.EMPLOYEE, new Employee());
         model.addAttribute(AttributeConstants.POSITIONS, positions);
+
         return "register";
     }
 
@@ -56,6 +64,56 @@ public class AdminController {
             employee.setPosition(position);
             employeeService.add(employee);
         }
+        return "redirect:/admin";
+    }
+
+    @Secured(value = {UserRoleConstants.ADMIN})
+    @RequestMapping(value = {"/create-project"}, method = RequestMethod.GET)
+    public String goToCreateProject(Model model) {
+        model.addAttribute(AttributeConstants.PROJECT, new Project());
+        return "create-project";
+    }
+
+    @Secured(value = {UserRoleConstants.ADMIN})
+    @RequestMapping(value = {"/create-project"}, method = RequestMethod.POST)
+    public String createProject(@Valid Project project,
+                                BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "create-project";
+        }
+        projectService.add(project);
+        return "redirect:/admin";
+    }
+
+    @Secured(value = {UserRoleConstants.ADMIN})
+    @RequestMapping(value = {"/add-employee-to-project"}, method = RequestMethod.GET)
+    public String goToAddEmployeeToProject(Model model) {
+        List<Project> projects = projectService.getAll();
+        List<Employee> employees = employeeService.getAll();
+        List<Role> roles = roleService.getAll();
+
+        model.addAttribute(AttributeConstants.MEMBER, new Member());
+        model.addAttribute(AttributeConstants.PROJECTS, projects);
+        model.addAttribute(AttributeConstants.EMPLOYEES, employees);
+        model.addAttribute(AttributeConstants.ROLES, roles);
+
+        return "add-employee-to-project";
+    }
+
+    @Secured(value = {UserRoleConstants.ADMIN})
+    @RequestMapping(value = {"/add-employee-to-project"}, method = RequestMethod.POST)
+    public String createProject(@Valid Member member,
+                                BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "add-employee-to-project";
+        }
+
+        //TODO на проект не может быть назначен один и тот же человек и админ
+        int employeeId = member.getEmployee().getId();
+        int projectId = member.getProject().getId();
+        int roleId = member.getRole().getId();
+        memberService.add(employeeId, projectId, roleId);
 
         return "redirect:/admin";
     }
