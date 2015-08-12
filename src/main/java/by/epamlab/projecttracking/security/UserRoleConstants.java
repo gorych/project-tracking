d5500fb0587cfc96d7b063494f6482d9f5cbb961
Egor Semenchenia <gorych911@gmail.com>
2015-08-12 17:36:32 +0300

    fixed dropdown bugs

--- src/main/java/by/epamlab/projecttracking/security/UserRoleConstants.java
+++ src/main/java/by/epamlab/projecttracking/security/UserRoleConstants.java
@@ -3,10 +3,14 @@ package by.epamlab.projecttracking.security;
 public final class UserRoleConstants {
 
     public static final String ADMIN_ROLE_NAME = "admin";
+    public static final String TEAM_LEAD_ROLE_NAME = "teamlead";
+    public static final String PR_MANAGER_ROLE_NAME = "project manager";
 
     public static final String ADMIN = "ROLE_ADMIN";
     public static final String USER = "ROLE_USER";
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+    public static final String TEAM_LEAD = "ROLE_TEAM_LEAD";
+    public static final String PR_MANAGER = "ROLE_PR_MANAGER";
 
     private UserRoleConstants(){ }
 
