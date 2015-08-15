409b68183375bffbab4482f30d44bf4604eb9c98
Egor Semenchenia <gorych911@gmail.com>
2015-08-15 13:44:04 +0300

    added page switcher

--- src/main/java/by/epamlab/projecttracking/web/controllers/IndexController.java
+++ src/main/java/by/epamlab/projecttracking/web/controllers/IndexController.java
@@ -2,25 +2,51 @@ package by.epamlab.projecttracking.web.controllers;
 
 
 import by.epamlab.projecttracking.security.UserRoleConstants;
+import by.epamlab.projecttracking.service.interfaces.EmployeeService;
 import by.epamlab.projecttracking.web.AttributeConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView goToLogin(@RequestParam(value = "auth_error", required = false) String error) {
-        ModelAndView model = new ModelAndView();
-        if (error != null) {
-            model.addObject(AttributeConstants.AUTHENTICATION_ERROR, "Wrong username or password.");
+    @Autowired
+    EmployeeService employeeService;
+
+    @RequestMapping(value = "/switch", method = RequestMethod.GET)
+    public String pageSwitcher() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        Collection roles = authentication.getAuthorities();
+
+        Iterator itr = roles.iterator();
+        if (itr.hasNext()) {
+            SimpleGrantedAuthority simpleRole = (SimpleGrantedAuthority) itr.next();
+            String role = simpleRole.getAuthority();
+            return UserRoleConstants.ADMIN.equals(role) ? "redirect:/admin" : "redirect:/dashboard";
         }
-        model.setViewName("login");
-        return model;
+
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String goToLogin(@RequestParam(value = "auth_error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute(AttributeConstants.AUTHENTICATION_ERROR, "Wrong username or password.");
+        }
+        return "login";
     }
 
     @Secured(UserRoleConstants.ANONYMOUS)
@@ -36,19 +62,13 @@ public class IndexController {
     }
 
     @RequestMapping(value = "/403", method = RequestMethod.GET)
-    public String roToAccessDenied() {
+    public String goToAccessDenied() {
         return "403";
     }
 
     @RequestMapping(value = "/404", method = RequestMethod.GET)
     public String goToResourceNotFound() {
         return "404";
-    }
-
-    @Secured(UserRoleConstants.USER)
-    @RequestMapping(value = {"/projects"}, method = RequestMethod.GET)
-    public String goToProjects() {
-        return "projects";
     }
 
 }
