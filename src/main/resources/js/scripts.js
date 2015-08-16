0fe24e0467dff4efe7fe3ef61aa1865470e6b2a2
Egor Semenchenia <gorych911@gmail.com>
2015-08-16 14:03:50 +0300

    added script for upload data to select

--- src/main/resources/js/scripts.js
+++ src/main/resources/js/scripts.js
@@ -18,3 +18,25 @@ $(document).ready(function () {
         });
     });
 });
+
+function test(select) {
+    var projectId = select.options[select.selectedIndex].value;
+    $.ajax({
+        url: 'loadProjectTeam',
+        method: 'GET',
+        data: {"projectId": projectId}
+    }).done(function (data) {
+        var members = JSON.parse(data);
+        if (data.length > 0) {
+            $('#membersSelect')
+                .find('option')
+                .remove()
+                .end();
+            $.each(members, function (index, element) {
+                $('#membersSelect')
+                    .append('<option value=' + element["id"] + '>' + element["fullName"] + '</option>')
+                    .val(element["id"]);
+            });
+        }
+    });
+}
