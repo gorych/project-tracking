6212aa0bca6daf786037c26f6de26377714466a2
Egor Semenchenia <gorych911@gmail.com>
2015-08-17 01:18:13 +0300

    added new attributes

--- src/main/java/by/epamlab/projecttracking/web/AttributeConstants.java
+++ src/main/java/by/epamlab/projecttracking/web/AttributeConstants.java
@@ -2,13 +2,12 @@ package by.epamlab.projecttracking.web;
 
 public final class AttributeConstants {
 
-    public static final String USER = "USER";
+    public static final String USER = "user";
+    public static final String TASK = "task";
     public static final String MEMBER = "member";
     public static final String PROJECT = "project";
     public static final String EMPLOYEE = "employee";
     public static final String ASSIGNMENT = "assignment";
-    public static final String TEAM_LEAD = "team_lead";
-
 
     public static final String TASKS = "tasks";
     public static final String MEMBERS = "members";
@@ -18,11 +17,11 @@ public final class AttributeConstants {
     public static final String PROJECTS = "projects";
     public static final String ROLES = "roles";
 
+    public static final String SUCCESS_MESSAGE= "success_msg";
     public static final String REGISTER_ERROR = "register_error";
     public static final String ADD_EMPLOYEE_TO_PROJECT_ERROR = "add_employee_to_project_error";
     public static final String AUTHENTICATION_ERROR = "auth_error";
-    public static final String ACCESS_DENIED_ERROR = "access_denied_error";
-    public static final String RESOURCE_NOT_FOUND_ERROR = "resource_not_found";
+    public static final String INPUT_DATE_ERROR = "input_date_error";
 
     private AttributeConstants() {
     }
