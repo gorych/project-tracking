fc3b33204cbdde87f4cf0397f9ce084c543b4c10
Egor Semenchenia <gorych911@gmail.com>
2015-08-19 20:42:33 +0300

    added constants class

---
+++ src/main/java/by/epamlab/projecttracking/web/Constants.java
@@ -0,0 +1,14 @@
+package by.epamlab.projecttracking.web;
+
+public final class Constants {
+
+    public static final String ROOT_DIR = "E://server/";
+    public static final String RESPONSE_CONTENT_TYPE = "application/octet-stream";
+    public static final String RESPONSE_HEADER_NAME = "Content-Disposition";
+    public static final String RESPONSE_HEADER_SUFFIX = "attachment; filename=";
+
+    private Constants(){
+
+    }
+
+}
