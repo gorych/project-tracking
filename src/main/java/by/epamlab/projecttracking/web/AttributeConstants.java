74fa080dcc15de5b41f95ef73881fcf58441ff1e
Egor Semenchenia <gorych911@gmail.com>
2015-08-19 20:51:41 +0300

    added new attribute constants

--- src/main/java/by/epamlab/projecttracking/web/AttributeConstants.java
+++ src/main/java/by/epamlab/projecttracking/web/AttributeConstants.java
@@ -9,15 +9,14 @@ public final class AttributeConstants {
     public static final String TASK = "task";
     public static final String PROJECT = "project";
     public static final String ASSIGNMENT = "assignment";
-
-    public static final String NEW_TASK = "new_task";
-    public static final String NEW_PROJECT = "new_project";
-    public static final String NEW_ASSIGNMENT = "new_assignment";
-    public static final String NEW_MEMBER = "new_member";
-    public static final String NEW_ACTIVITY = "new_activity";
-    public static final String NEW_EMPLOYEE = "new_employee";
+    public static final String ATTACHMENT = "attachment";
+    public static final String MEMBER = "member";
+    public static final String ACTIVITY = "activity";
+    public static final String EMPLOYEE = "employee";
 
     public static final String TASKS = "tasks";
+    public static final String TASKS_ID = "task_id";
+    public static final String PROJECT_ID = "project_id";
     public static final String PROJECT_MEMBERS = "members";
     public static final String USER_MEMBERS = "user_members";
     public static final String ACTIVITIES = "activities";
@@ -26,6 +25,7 @@ public final class AttributeConstants {
     public static final String PROJECTS = "projects";
     public static final String ROLES = "roles";
 
+    public static final String UPLOAD_FILE_ERROR = "upload_error";
     public static final String REGISTER_ERROR = "register_error";
     public static final String ADD_EMPLOYEE_TO_PROJECT_ERROR = "add_employee_to_project_error";
     public static final String AUTHENTICATION_ERROR = "auth_error";
