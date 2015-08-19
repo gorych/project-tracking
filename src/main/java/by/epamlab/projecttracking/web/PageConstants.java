e2de32ba0c315066468e585aa8e6a4de27d2ef4c
Egor Semenchenia <gorych911@gmail.com>
2015-08-19 20:54:01 +0300

    added new constants

--- src/main/java/by/epamlab/projecttracking/web/PageConstants.java
+++ src/main/java/by/epamlab/projecttracking/web/PageConstants.java
@@ -2,6 +2,30 @@ package by.epamlab.projecttracking.web;
 
 public final class PageConstants {
 
+    public static final String RESOURCE_NOT_FOUND = "404";
+    public static final String ACCESS_DENIED = "403";
+    public static final String DOWNLOAD = "download";
+    public static final String PAGE_SWITCH = "switch";
+    public static final String STATUS_SWITCHER = "status-switcher";
+    public static final String ROOT = "/";
+    public static final String USER = "/user";
+    public static final String ADMIN = "admin";
+    public static final String ASSIGN = "assign";
+    public static final String ISSUES = "issues";
+    public static final String LOGIN = "login";
+    public static final String REPORT = "report";
+    public static final String EXPORT = "export";
+    public static final String ADMIN_PANEL = "admin-panel";
+    public static final String ADMIN_PAGE = "admin-page";
+    public static final String DASHBOARD = "dashboard";
+    public static final String USER_DASHBOARD = "user/dashboard";
+    public static final String ACTIVITY = "activity";
+    public static final String PROJECTS = "projects";
+    public static final String REGISTER = "register";
+    public static final String CREATE_PROJECT = "create-project";
+    public static final String ADD_EMPLOYEE_TO_PROJECT = "add-employee-to-project";
+    public static final String CREATE_ISSUE = "create-issue";
+
     private PageConstants() {
     }
 
