a73d7b7e5d52997499165f66d6deb36cf8a99560
Egor Semenchenia <gorych911@gmail.com>
2015-08-20 12:38:15 +0300

    added new constants

--- src/main/java/by/epamlab/projecttracking/web/Constants.java
+++ src/main/java/by/epamlab/projecttracking/web/Constants.java
@@ -2,14 +2,16 @@ package by.epamlab.projecttracking.web;
 
 public final class Constants {
 
-    public static final int BUFFER_SIZE = 1024;
+    public static final String ANONYMOUS = "anonymous";
+
+    public static final int STATUS_IN_PROGRESS = 2;
+
     public static final String ROOT_DIR = "E://server/";
     public static final String RESPONSE_CONTENT_TYPE = "application/octet-stream";
     public static final String RESPONSE_HEADER_NAME = "Content-Disposition";
     public static final String RESPONSE_HEADER_SUFFIX = "attachment; filename=";
 
-    private Constants(){
-
+    private Constants() {
     }
 
 }
